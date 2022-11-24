43f26e915a879a806c5a72e355bbe780de92b7c1
Alexandr Mun <mun_a2006@yahoo.de>
2022-11-24 17:34:56 +0600

    added securityFilterChain

--- src/main/java/kz/bitlab/spring/crm/config/SecurityConfig.java
+++ src/main/java/kz/bitlab/spring/crm/config/SecurityConfig.java
@@ -2,48 +2,45 @@ package kz.bitlab.spring.crm.config;
 
 import kz.bitlab.spring.crm.controllers.Constants;
 import kz.bitlab.spring.crm.services.UsersService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, proxyTargetClass = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@RequiredArgsConstructor
+public class SecurityConfig {
 
-    @Autowired
-    private UsersService usersService;
+    private final UsersService usersService;
 
-    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(usersService);
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http.authorizeRequests().antMatchers("/").permitAll();
-
-        http.exceptionHandling().accessDeniedPage("/403");
-
-        http.formLogin()
-                .loginPage(Constants.API_USERS + "/login").permitAll()
-                .usernameParameter("user_email")
-                .passwordParameter("user_password")
-                .loginProcessingUrl("/auth").permitAll()
-                .failureUrl(Constants.API_USERS + "/login?error")
-                .defaultSuccessUrl(Constants.API_USERS + "/profile");
-
-        http.logout()
-                .logoutUrl("/logout").permitAll()
-                .logoutSuccessUrl(Constants.API_USERS + "/login");
-
-        http.csrf().disable();
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .antMatchers("/").permitAll()
+                )
+                .exceptionHandling((exception) -> exception.accessDeniedPage("/403")
+                )
+                .formLogin((login) -> login.loginPage((Constants.API_USERS + "/login")).permitAll()
+                        .usernameParameter("user_email")
+                        .passwordParameter("user_password")
+                        .loginProcessingUrl("/auth").permitAll()
+                        .failureUrl(Constants.API_USERS + "/login?error")
+                        .defaultSuccessUrl(Constants.API_USERS + "/profile")
+                )
+                .logout((logout) -> logout.logoutUrl("/logout").permitAll()
+                        .logoutSuccessUrl(Constants.API_USERS + "/login")
+                );
+        return http.build();
     }
 }
